68b7fa315586f2cc95f563055123b1b9db5026fc
Aidan Feldman <aidan.feldman@gmail.com>
2025-06-13 12:08:37 -0400

    expand recurring events

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem "csv"
 gem "icalendar"
+gem "rrule"
--- rehearsals.rb
+++ rehearsals.rb
@@ -1,5 +1,6 @@
-require "icalendar"
 require "csv"
+require "icalendar"
+require "rrule"
 
 START_FROM = DateTime.new(2024, 7, 28)
 MY_EMAIL = "aidan.feldman@gmail.com"
@@ -46,6 +47,37 @@ def format_time(time)
   time.in_time_zone("America/New_York").strftime("%m/%d/%Y %H:%M:%S")
 end
 
+def expand_recurring_events(events)
+  expanded_events = []
+
+  events.each do |event|
+    if event.rrule.empty?
+      # Non-recurring event
+      expanded_events << event
+    else
+      # Recurring event - expand occurrences
+      rrule_string = event.rrule.first.value_ical
+      start_time = event.dtstart.to_time
+      end_time = event.dtend.to_time
+      duration = end_time - start_time
+
+      # Parse the rrule and generate occurrences
+      rrule = RRule.parse(rrule_string, dtstart: start_time)
+      occurrences = rrule.between(start_time, Time.now)
+
+      occurrences.each do |occurrence_start|
+        # Create a new event for each occurrence
+        occurrence_event = event.dup
+        occurrence_event.dtstart = occurrence_start
+        occurrence_event.dtend = occurrence_start + duration
+        expanded_events << occurrence_event
+      end
+    end
+  end
+
+  expanded_events
+end
+
 # gets set to US-ASCII when run through rdbg
 Encoding.default_external = "UTF-8"
 
@@ -57,7 +89,9 @@ CSV.open("rehearsals.csv", "w") do |csv|
   csv << ["Name", "Location", "Start", "End", "Hours"]
 
   events = cal.events.sort_by(&:dtstart)
-  events.each do |event|
+  expanded_events = expand_recurring_events(events)
+
+  expanded_events.each do |event|
     if eligible?(event)
       row = [
         event.summary,
